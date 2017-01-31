The MoM code is working correctly. Fix the meshing, populating the matrices and scatter calculation. Added plot for mesh and scatter.

create_mesh.py
@@ -1,164 +1,54 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 17 14:53:38 2017
+Created on Mon Jan 30 20:27:49 2017
 
-@author: mrichardson
+@author: michael
 """
 
+import numpy as np
 import parameters as params
-import point
-import segment
-import math
+
+length = params.line_length
+elem_size = params.meshsize_solve
 
 def create_line():
-    side_length = 20*params.LAM_0
+    num_elem = int(round(length/elem_size))+1
+    node_coords = np.zeros((num_elem, 2))
+    curr_node = np.array([0, -length/2])
         
-    m_length = params.MAX_MESH_SIZE
-    mesh = []
+    for ii in range(0, num_elem):
+        node_coords[ii] = curr_node
+        curr_node = np.array([0, curr_node[1] + elem_size])
+        
+    elem_nodes = np.zeros((num_elem, 2))
     
-    n_segs = int(side_length/m_length)
-
-    for ii in range(0, n_segs/2):
-        if ii == 0:
-            p_start = point.Point(0.0, 0.0)
-            n_start = point.Point(0.0, 0.0)
-        else:
-            start_val = ii*m_length
-            p_start = point.Point(0.0, start_val)
-            n_start = point.Point(0.0, -start_val)
-            
-        next_val = (ii+1)*m_length
-        p_end = point.Point(0.0, next_val)
-        n_end = point.Point(0.0, -next_val)
-        
-        p_seg = segment.Segment(p_start, p_end)
-        n_seg = segment.Segment(n_start, n_end)
-        
-        mesh.append(p_seg)
-        mesh.append(n_seg)
-        
-    return mesh
-
+    for ii in range(0, num_elem):
+        elem_nodes[ii] = np.array([ii, ii+1])
+    
+    return {"num_elem": num_elem, "node_coords": node_coords, "elem_nodes": elem_nodes}
+    
 def create_reflector():
-    side_length = 10*params.LAM_0
-    m_length = params.MAX_MESH_SIZE
-    n_segs = int(side_length/m_length)
+    num_elem = 2*(int(round(length/elem_size)))
+    node_coords = np.zeros((num_elem, 2))
+    curr_node = np.array([0, 0])
     
-    mesh = []
-       
-    for ii in range(0, n_segs):
-        if ii == 0:
-            p_start = point.Point(0.0, 0.0)
-            n_start = point.Point(0.0, 0.0)
-        else:
-            start_val = ii*m_length
-            p_start = point.Point(start_val, start_val)
-            n_start = point.Point(start_val, -start_val)
-            
-        next_val = (ii+1)*m_length
-        p_end = point.Point(next_val, next_val)
-        n_end = point.Point(next_val, -next_val)
-        
-        p_seg = segment.Segment(p_start, p_end)
-        n_seg = segment.Segment(n_start, n_end)
-        
-        mesh.append(p_seg)
-        mesh.append(n_seg)
+    count = 0
     
-    return mesh
+    for ii in range(0, int(num_elem/2)):
+        node_coords[count] = curr_node
+        curr_node = np.array([(ii+1)*elem_size, (ii+1)*elem_size])
+        count = count + 1
     
-def create_circle():
-    radius = 10*params.LAM_0
-    circum = 2*params.PI*radius
-    m_length = params.MAX_MESH_SIZE
-    n_segs = int(circum/m_length)
-    ang_inc = 2*params.PI/n_segs
+    curr_node = np.array([0, 0])
+        
+    for ii in range(0, int(num_elem/2)):
+        node_coords[count] = curr_node
+        curr_node = np.array([(ii+1)*elem_size, -1*(ii+1)*elem_size])
+        count = count + 1
     
-    mesh = []
-       
-    for ii in range(0, n_segs):
-        if ii == 0:
-            start = point.Point(radius, 0.0)
-        else:
-            start_ang = ii*ang_inc
-            start = point.Point(radius*math.cos(start_ang), radius*math.sin(start_ang))
-            
-        next_ang = (ii+1)*ang_inc
-        end = point.Point(radius*math.cos(next_ang), radius*math.sin(next_ang))
-        
-        seg = segment.Segment(start, end)
-        
-        mesh.append(seg)
+    elem_nodes = np.zeros((num_elem, 2))
     
-    return mesh
-
-def create_line_with_angled_edge():
-    side_length = 10*params.LAM_0
-    ang_length = 5*params.LAM_0
-    obs_length = 8*params.LAM_0
-    dist_away = 4*params.LAM_0
-    shift_obs = 1*params.LAM_0
-    
-    m_length = params.MAX_MESH_SIZE
-    mesh = []
-    
-    n_segs = int(side_length/m_length)
-
-    for ii in range(0, n_segs/2):
-        if ii == 0:
-            p_start = point.Point(0.0, 0.0)
-            n_start = point.Point(0.0, 0.0)
-        else:
-            start_val = ii*m_length
-            p_start = point.Point(0.0, start_val)
-            n_start = point.Point(0.0, -start_val)
-            
-        next_val = (ii+1)*m_length
-        p_end = point.Point(0.0, next_val)
-        n_end = point.Point(0.0, -next_val)
+    for ii in range(0, num_elem):
+        elem_nodes[ii] = np.array([ii, ii+1])
         
-        p_seg = segment.Segment(p_start, p_end)
-        n_seg = segment.Segment(n_start, n_end)
-        
-        mesh.append(p_seg)
-        mesh.append(n_seg)
-        
-    n_segs = int(ang_length/m_length)
-    
-    for ii in range(0, n_segs):
-        if ii == 0:
-            start = point.Point(0.0, ang_length)
-        else:
-            start_val = ii*m_length
-            start = point.Point(start_val, start_val + ang_length)
-            
-        next_val = (ii+1)*m_length
-        end = point.Point(next_val, next_val + ang_length)
-        
-        seg = segment.Segment(start, end)
-        
-        mesh.append(seg)
-        
-    n_segs = int(obs_length/m_length)
-    
-    for ii in range(0, n_segs/2):
-        if ii == 0:
-            p_start = point.Point(dist_away, 0.0 + shift_obs)
-            n_start = point.Point(dist_away, 0.0 + shift_obs)
-        else:
-            start_val = ii*m_length
-            p_start = point.Point(dist_away, start_val + shift_obs)
-            n_start = point.Point(dist_away, -start_val + shift_obs)
-            
-        next_val = (ii+1)*m_length
-        p_end = point.Point(dist_away, next_val + shift_obs)
-        n_end = point.Point(dist_away, -next_val + shift_obs)
-        
-        p_seg = segment.Segment(p_start, p_end)
-        n_seg = segment.Segment(n_start, n_end)
-        
-        mesh.append(p_seg)
-        mesh.append(n_seg)
-        
-    return mesh
-    
+    return {"num_elem": num_elem, "node_coords": node_coords, "elem_nodes": elem_nodes}

parameters.py
@@ -1,53 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 17 10:18:21 2017
+Created on Mon Jan 30 20:44:57 2017
 
-@author: mrichardson
+@author: michael
 """
 
-import math
+import numpy as np
 
-# Debug level
-DEBUG          = 1
+pi              = np.pi
+eps0            = 8.854e-12
+mu0             = 4*pi*1e-7
+c0              = 2.99792458e8
+freq            = 300e6
+freq_range      = [150e6, 50e6, 450e6]
+lam0            = c0/freq
+k0              = 2*pi/lam0
+eta0            = np.sqrt(mu0/eps0)
+gam             = 1.781072418
+phi_inc         = 0
 
-# Frequency of simulation
-FREQUENCY      = 1e9;
+num_fieldpoints = 360
+phi_fieldpoints = np.arange(0, pi, pi/num_fieldpoints)
+rad_fieldpoints = 100*lam0
+line_length     = 10*lam0
 
-# Incident angle of plane wave
-INCIDENT_ANGLE = 0;
+meshsize_solve  = lam0/10
 
-# Gaussian quadrature setting
-GAUSSIAN_QUADRATURE = 5
-
-# Number of reflections for methods
-NUM_MRPO_REFLECTIONS = 7
-MAX_IPO_REFLECTIONS = 30
-    
-# Electromagnetic constants
-E_0            = 1
-PI             = 3.14159265358979323846;
-EPS_0          = 8.854e-12;
-MU_0           = PI*4e-7;
-C_0            = 1/math.sqrt(EPS_0*MU_0);
-LAM_0          = C_0/FREQUENCY;
-K_0            = 2*PI/LAM_0;
-ETA_0          = math.sqrt(MU_0/EPS_0);
-OMEGA          = 2*PI*FREQUENCY;
-GAM_0          = 1.781072418
-
-# Maximum size of mesh element
-MAX_MESH_SIZE  = LAM_0/10;
-
-# Value for zero (computational errors)
-ZERO           = 1e-6
-
-# Start phi value
-PHI_START      = 0;
-# End phi value
-PHI_END        = 180;
-# Increment phi value
-PHI_INCREMENT  = 1;
-# Phi radius
-PHI_RADIUS     = 1000*LAM_0;
-
-PLANE_WAVE_SHADOW_DIST = 100*LAM_0
+plot_angs       = np.arange(0, 180, 0.5)

plot.py
@@ -1,47 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 17 15:26:13 2017
+Created on Tue Jan 31 12:28:40 2017
 
-@author: mrichardson
+@author: michael
 """
 
 import matplotlib.pyplot as plt
 import parameters as params
-import numpy as np
 
-def plot_mesh(mesh, n=1):
-    lam_0 = params.LAM_0
-    
-    for ii in range(0, len(mesh)):
-        s_start = mesh[ii].getStart()
-        s_end = mesh[ii].getEnd()
-                
-        plt.plot([s_start.getX()/lam_0, s_end.getX()/lam_0], [s_start.getY()/lam_0, s_end.getY()/lam_0], '-r')
-    
-    plt.figure(n)    
-    plt.ylabel('y-coordinate [$\lambda_0$]')
-    plt.xlabel('x-coordinate [$\lambda_0$]')
+def plot_mesh(node_coords):
+    plt.figure(1)
+    plt.title('Mesh')
+    plt.xlabel(r'x-coordinates [$\lambda_{0}$]')
+    plt.ylabel(r'y-coordinates [$\lambda_{0}$]')
+    plt.plot(node_coords[:,0], node_coords[:,1], '-r')
     plt.axis('equal')
     x1,x2,y1,y2 = plt.axis()
     plt.axis((x1,x2,y1 - 2 ,y2 + 2))
-    plt.grid()
+    plt.grid(True)
     plt.show()
     
-def plot_data(data, n=2):
-    ang = np.arange(0, len(data), 1)
     
-    plt.figure(n)
-    
+def plot_scatter(scattering):
+    plt.figure(2)
+    plt.title('2D RCS Real')
     plt.xlabel(r'$\phi$ [degrees]')
-    
-    #mrpo_plot, = plt.plot(ang, data, label='MRPO', linestyle='-', marker='D', ms=4, markevery=3)
-    mrpo_plot, = plt.plot(ang, data, label='MRPO')
-    
-    plt.grid(True)
-    
     plt.ylabel(r'$\sigma_{2D}$ [dB]')
-#    plt.legend(handles=[mrpo_plot], bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-#       ncol=3, mode="expand", borderaxespad=0.)
-#    plt.savefig(savePath + "updated_combined_rcs.eps", format='eps', dpi=1200, bbox_inches='tight')
-#    print("Saved figure as " + savePath + "updated_combined_rcs.eps")
-    print('Image plotted...')
+    plt.plot(params.plot_angs, scattering.real, label='MoM')
+    plt.grid(True)
+    plt.show()